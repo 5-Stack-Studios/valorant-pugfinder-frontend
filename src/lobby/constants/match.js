2955a34fead09751ea0fa365996351439bd68d83
Patrick Gallagher <pjg4@illinois.edu>
2020-05-21 15:43:05 -0500

    add basic lobby action constants

---
+++ src/lobby/constants/match.js
@@ -0,0 +1,4 @@
+import makeActionStrings from "../../common/actionUtils.js";
+
+export const END_GAME = makeActionStrings("END_GAME");
+export const REPORT_MATCH_STATS = makeActionStrings("REPORT_MATCH_STATS");
---
+++ src/lobby/constants/players.js
@@ -0,0 +1,5 @@
+import makeActionStrings from "../../common/actionUtils.js";
+
+export const CHANGE_SELF_ROLES = makeActionStrings("CHANGE_SELF_ROLES");
+export const CHANGE_READY_STATE = makeActionStrings("CHANGE_READY_STATE");
+export const CHANGE_LINEUP = makeActionStrings("CHANGE_LINEUP");
