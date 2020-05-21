adbf835ed2ddffdb93f16f548ef865a38bb40aa4
Patrick Gallagher <pjg4@illinois.edu>
2020-05-21 18:04:34 -0500

    add helper to create action function

--- src/common/actionUtils.js
+++ src/common/actionUtils.js
@@ -3,3 +3,18 @@ export const makeActionStrings = action => ({
     SUCCESS: `${action}_SUCCESS`,
     FAILURE: `${action}_FAILURE`
 });
+
+/**
+ * Creates an action function for the specified type that
+ * generates an action object with those params
+ * @param {String} type 
+ * @param  {...String} params 
+ */
+export const makeActionFunction = (type, ...params) =>
+    function(...args) {
+        const action = { type, payload: {} };
+        params.forEach((arg, i) => {
+            action.payload[params[i]] = args[i]
+        });
+        return action;
+    }
