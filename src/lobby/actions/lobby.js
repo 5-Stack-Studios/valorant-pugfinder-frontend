3fe19ff5c760b35c9c068fc390a468de623d0bd1
Patrick Gallagher <pjg4@illinois.edu>
2020-05-21 18:05:05 -0500

    initialize lobby action functions

---
+++ src/lobby/actions/lobby.js
@@ -0,0 +1,48 @@
+import { 
+    CREATE_LOBBY,
+} from "../constants/lobby";
+
+import { makeActionFunction } from "../../common/actionUtils";
+
+/**
+ * Creates an action attempting to create a lobby
+ * @param {String} userId the creator of the lobby
+ * @param {Object} lobbyParams the settings for the lobby
+ */
+export const createLobbyRequest = makeActionFunction(
+    CREATE_LOBBY.REQUEST,
+    'userId',
+    'lobbyParams'
+);
+
+/**
+ * Creates a success action for after lobby is created
+ * @param {String} userId the creator of the lobby
+ * @param {Object} lobbyData the returned lobby data
+ */
+export const createLobbySuccess = makeActionFunction(
+    CREATE_LOBBY.SUCCESS,
+    'userId',
+    'lobbyData'
+);
+
+// TODO: Brainstorm a better way to get error in here
+/**
+ * Creates a failure action for when the lobby failed
+ * to create
+ * @param {String} userId
+ */
+export const createLobbyFailure = makeActionFunction(
+    CREATE_LOBBY.FAILURE,
+    'userId',
+    'error'
+);
+
+/**
+ * Fires off a lobby creation request to the server
+ * @param {String} userId the creator of the lobby
+ * @param {Object} lobbyParams the settings for the lobby
+ */
+export const fetchQuestion = (userId, lobbyParams) => dispatch => {
+    // TODO: Implement with axios
+};
