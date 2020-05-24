7db4eeba93ad3b3c4e9765b124570a07750bf38a
Patrick Gallagher <pjg4@illinois.edu>
2020-05-24 00:19:10 -0500

    add close lobby test

--- src/lobby/tests/lobby.test.js
+++ src/lobby/tests/lobby.test.js
@@ -1,35 +1,121 @@
-import * as reducers from '../reducers/lobby';
-import * as LOBBY from '../constants/lobby';
+import * as reducers from "../reducers/lobby";
+import * as LOBBY from "../constants/lobby";
 
-describe('lobbies reducer', () => {
-  it('should return the initial state', () => {
+describe("lobbies reducer", () => {
+  it("should return the initial state", () => {
     expect(reducers.lobbies(undefined, {})).toEqual([]);
   });
 
-  it('should be able to add one lobby', () => {
+  it("should be able to add one lobby", () => {
     expect(
       reducers.lobbies(undefined, {
         type: LOBBY.CREATE_LOBBY.SUCCESS,
         payload: {
           lobbyData: {
-            owner: '1',
-            map: 'bind',
-            id: 'abcd123'
-          }
-        }
+            owner: "1",
+            map: "bind",
+            id: "abcd123",
+          },
+        },
       })
     ).toEqual([
       {
         team1: [],
         team2: [],
         captains: [],
-        map: 'bind',
-        owner: '1',
-        id: 'abcd123',
+        map: "bind",
+        owner: "1",
+        id: "abcd123",
         queue: {
-            // TODO: define game roles in a constant/get from Riot
-        }
-      }
+          // TODO: define game roles in a constant/get from Riot
+        },
+      },
     ]);
-  })
-})
+  });
+
+  it("should be able to append a new lobby to current", () => {
+    const currentLobbies = [
+      {
+        team1: [],
+        team2: [],
+        captains: [],
+        map: "bind",
+        owner: "1",
+        id: "abcd123",
+        queue: {},
+      },
+    ];
+    expect(
+      reducers.lobbies(currentLobbies, {
+        type: LOBBY.CREATE_LOBBY.SUCCESS,
+        payload: {
+          lobbyData: {
+            owner: "1",
+            map: "haven",
+            id: "xyz435",
+          },
+        },
+      })
+    ).toEqual([
+      {
+        team1: [],
+        team2: [],
+        captains: [],
+        map: "bind",
+        owner: "1",
+        id: "abcd123",
+        queue: {},
+      },
+      {
+        team1: [],
+        team2: [],
+        captains: [],
+        map: "haven",
+        owner: "1",
+        id: "xyz435",
+        queue: {},
+      },
+    ]);
+  });
+
+  it("should be able to remove by lobby ID", () => {
+    const currentLobbies = [
+      {
+        team1: [],
+        team2: [],
+        captains: [],
+        map: "bind",
+        owner: "1",
+        id: "abcd123",
+        queue: {},
+      },
+      {
+        team1: [],
+        team2: [],
+        captains: [],
+        map: "haven",
+        owner: "1",
+        id: "xyz435",
+        queue: {},
+      },
+    ];
+    expect(
+      reducers.lobbies(currentLobbies, {
+        type: LOBBY.CLOSE_LOBBY.SUCCESS,
+        payload: {
+          lobbyId: "xyz435",
+        },
+      })
+    ).toEqual([
+      {
+        team1: [],
+        team2: [],
+        captains: [],
+        map: "bind",
+        owner: "1",
+        id: "abcd123",
+        queue: {},
+      },
+    ]);
+  });
+});
