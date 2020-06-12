f039259f07ab8225f50c5e3aab5e47cafb68a06e
Patrick Gallagher <pjg4@illinois.edu>
2020-06-11 23:59:09 -0500

    remove default lobby

--- src/App.js
+++ src/App.js
@@ -2,20 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import CreateLobbyPanel from "./lobby/containers/CreateLobbyPanel";
 import LobbyContainer from "./lobby/containers/LobbyContainer";
-import { createLobbySuccess } from "./lobby/actions/lobby";
 
 class App extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(
-      createLobbySuccess({
-        map: "split",
-        owner: "1",
-        id: 0,
-      })
-    );
-    console.log("Dispatched");
-  }
-
   render() {
     const { lobbies } = this.props;
     console.log("Rerender", lobbies);
