7bcf92eb45e302677d1d8db54590e128835cfe3b
Patrick Gallagher <pjg4@illinois.edu>
2020-07-02 22:23:58 -0500

    tweak lobby styles

--- src/lobby/components/PlayerSlot.js
+++ src/lobby/components/PlayerSlot.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./PlayerSlot.css";
+import { AGENTS_TO_PORTRAITS } from "../constants/players";
 /**
  * A small, rectangular div that shows the player's name and role
  * Should look somewhat like the role select screen in Valorant.
  * @param {Object} player the player. contains name and role
  * 
  */
-const PlayerSlot = ({playerName, selectedRole}) => (
-    <div className="player-slot">
-        <h2>{playerName}</h2>
+const PlayerSlot = ({name, selectedRole, team}) => (
+    <div className={`player-slot ${team}`}>
+        <h3 className="player-name">{name}</h3>
         <div className = "character-selected">
-            <h3>{selectedRole}</h3>
+            <img src={AGENTS_TO_PORTRAITS[selectedRole]}></img>
         </div>
     </div>
     //Put character image and map image here
