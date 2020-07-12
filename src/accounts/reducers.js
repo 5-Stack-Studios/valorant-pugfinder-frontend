1d7d88cb32e85b457536f8c200611e2c4c7595fb
Patrick Gallagher <pjg4@illinois.edu>
2020-07-11 19:23:17 -0500

    add in chokentime's reducer

--- src/accounts/reducers.js
+++ src/accounts/reducers.js
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
-import { SIGNUP, LOGIN } from "./actions";
+import { SIGNUP, LOGIN } from "./constants";
 
 const activeUser = (state = null, action) => {
   switch (action.type) {
+    case LOGIN.success:
+    case SIGNUP.success:
+      return action.payload;
+    default:
+      return state;
   }
-
-  return state;
 };
 
 const accounts = combineReducers({
