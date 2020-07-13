f30326e2c78b4995944dad51e0a5601ddcd759d1
Patrick Gallagher <pjg4@illinois.edu>
2020-07-12 21:56:48 -0500

    store account info in cookie

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
+import reduxCookiesMiddleware from "redux-cookies-middleware";
+import { getStateFromCookies } from "redux-cookies-middleware";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
@@ -13,9 +15,26 @@ import { rootSaga } from "./sagas";
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const sagaMiddleware = createSagaMiddleware();
+const cookiePaths = {
+  "accounts.token": { name: "valorant_pugfinder_token" },
+  "accounts.activeUser": { name: "valorant_pugfinder_user" },
+};
+
+let initialState = {
+  accounts: {
+    token: null,
+    activeUser: null,
+  },
+};
+initialState = getStateFromCookies(initialState, cookiePaths);
+
 const store = createStore(
   appReducer,
-  composeEnhancer(applyMiddleware(sagaMiddleware))
+  initialState,
+  composeEnhancer(
+    applyMiddleware(sagaMiddleware),
+    applyMiddleware(reduxCookiesMiddleware(cookiePaths))
+  )
 );
 
 ReactDOM.render(
